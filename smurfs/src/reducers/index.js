b1467a4c483e41febf96866f3e45cbb494269e37
coriagogo <pkcparis@gmail.com>
2019-05-17 11:42:12 -0400

    reducers started

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,17 +2,36 @@
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+import {
+  GET_SMURFS,
+  GET_SMURFS_SUCCESS,
+  GET_SMURFS_FAILURE,
+  ADD_SMURFS,
+  ADD_SMURFS_SUCCESS,
+  ADD_SMURFS_FAILURE,
+  UPDATE_SMURFS,
+  UPDATE_SMURFS_SUCCESS,
+  UPDATE_SMURFS_FAILURE,
+  DELETE_SMURFS,
+  DELETE_SMURFS_SUCCESS,
+  DELETE_SMURFS_FAILURE,
+} from '../actions';
+
+
+//  Your initial/default state for this project could *Although does not have to* look a lot like this
+const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+   updatingSmurf: false,
+   deletingSmurf: false,
    error: null
  }
-*/
+
+ const reducer = (state = initialState, action) => {
+   switch(action.type)
+ }
+
 
 /*
   You'll only need one smurf reducer for this project.
