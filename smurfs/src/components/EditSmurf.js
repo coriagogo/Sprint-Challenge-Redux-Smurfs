739176624769b0099e01ebad3e4f6d14e6f0834a
coriagogo <pkcparis@gmail.com>
2019-05-17 14:07:07 -0400

    removed extraneous files

--- smurfs/src/components/EditSmurf.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const EditSmurf = () => {
-    <div className="edit-card">
-        <input>{smurf.name}</input>
-        <input>{smurf.age}</input>
-        <input>{smurf.height}</input>
-        
-}
